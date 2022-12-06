01f3c3934a34aa081a3514216be9427e0728d460
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-05 22:47:53 -0700

    Create a skeleton for signup context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,17 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make API request to signup with that email and password
+    
+    // if we sign up, moify our state, ans say that we are authenticated
+
+    // if signing up fails, we probably need to reflect an error message somewhere
+
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
   {},
