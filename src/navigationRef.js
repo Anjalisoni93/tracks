f1206affdc205087ee1bf9616f0e961bb62e2d47
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-14 17:41:51 -0700

    Add a routename and params to the naigate actions

--- src/navigationRef.js
+++ src/navigationRef.js
@@ -1,6 +1,18 @@
+import { NavigationActions } from "react-navigation";
+
 let navigator;
 
 export const setNavigator = (nav) => {
   navigator = nav;
 };
 
+// routeName is names of routes/screens like Signup, Signin etc.
+// Params are some info to pass to the screen that we are about to show
+export const navigate = (routeName, params) => {
+  navigator.dispatch(
+    NavigationActions.navigate({
+      routeName,
+      params
+    })
+  );
+};
