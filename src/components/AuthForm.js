3aed625ecc0fa5d83686a0c92cd975080cd87df4
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-15 23:18:52 -0700

    Add teh style fro errorMessage

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button, Input } from "react-native-elements";
+import Spacer from "./Spacer";
 
 const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
   const [email, setEmail] = useState('');
@@ -44,7 +45,10 @@ const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
 
 const styles = StyleSheet.create({
   errorMessage: {
-    
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 
