50456a713cd1479c3296369daab74e1d5ed5d2b1
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-15 23:04:33 -0700

    Add the extracted logic from Signup scrren to the Auth Form

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button, Input } from "react-native-elements";
 
 const AuthForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Spacer />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
+      <Spacer>
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
+      </Spacer>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
