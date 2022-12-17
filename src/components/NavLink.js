9d3c39f63fa3bfcd7ee41246a2de5bd949a39e8d
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-16 22:07:34 -0700

    Add the extracted code from SignUp screen

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,8 +3,14 @@ import { Text, TouchableOpacity, StyleSheet } from "react-native-elements";
 import Spacer from "./Spacer";
 import { withNavigation } from "react-navigation";
 
-const NavLink = () => {
-
+const NavLink = ({ navigation }) => {
+  return (
+    <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+        <Spacer>
+          <Text style={styles.link}>Already have an account? Sign in instead</Text>
+        </Spacer>
+      </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({});
