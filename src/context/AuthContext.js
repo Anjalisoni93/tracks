119dc04e4ca7017d9b5a0b269917918239c8ec15
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-22 23:03:07 -0700

    Export the function to make ot avilable

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -76,6 +76,8 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup, clearErrorMessage },
+  {
+    signin, signout, signup, clearErrorMessage, tryLocalSignin
+  },
   { token: null, errorMessage: '' }
 );
