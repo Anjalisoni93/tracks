5716c0044204b0f3ffc68027652031b9df1011ce
Anjali Soni <anjalipatadiya@gmail.com>
2022-12-29 18:30:58 -0700

    Refactor

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,9 +10,9 @@ const authReducer = (state, action) => {
     case 'signin':
       return { errorMessage: '', token: action.payload };
     case "clear_error_message":
-      return { ...state, errorMessage: "" };
-    case "signout":
-      return { token: null, errorMessage: "" };
+      return { ...state, errorMessage: '' };
+    case 'signout':
+      return { token: null, errorMessage: '' };
     default:
       return state;
   }
@@ -72,7 +72,7 @@ const signin = (dispatch) => async ({ email, password }) => {
 
 const signout = dispatch => async () => {
   await AsyncStorage.removeItem('token');
-  dispatch({ type: 'signout' });
+  dispatch({ type: 'signput' });
   navigate('loginFlow');
 };
 
