f8a82095e794a2eb31f9508c9bd01dddea65f735
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-08 18:38:25 -0700

    Export all three functions and wrap them with context and provider

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -10,3 +10,13 @@ const locationReducer = (state, action) => {
 const startRecording = dispatch => () => {};
 const stopRecording = dispatch => () => {};
 const addLocation = dispatch => () => {};
+
+export const { Context, Provider } = createDataContext(
+  locationReducer,
+  {
+    startRecording,
+    stopRecording,
+    addLocation
+  },
+  { recording: false, locations: [], currentLocation: null }
+);
