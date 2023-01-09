4676e7a2039a9bce304cb779be74ba49cc32a125
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-08 18:25:07 -0700

    Change the latitude and longitude

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,13 +7,13 @@ const Map = () => {
   for (let i = 0; i < 20; i++) {
     if (i % 2 === 0) {
       points.push({
-        latitude: 37.33233 + i * 0.001,
-        longitude: -122.03121 + i * 0.001
+        latitude: 51.18438069389881 + i * 0.001,
+        longitude: -114.0546546181831 + i * 0.001
       });
     } else {
       points.push({
-        latitude: 37.33233 - i * 0.002,
-        longitude: -122.03121 + i * 0.001
+        latitude: 51.18438069389881 - i * 0.002,
+        longitude: -114.0546546181831 + i * 0.001
       });
     }
   }
@@ -22,8 +22,8 @@ const Map = () => {
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        latitude: 51.18438069389881,
+        longitude: -114.0546546181831,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
