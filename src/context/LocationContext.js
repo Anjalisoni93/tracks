c0da70a09e564cfa55966904942ccc9136a15f2b
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-09 21:08:33 -0700

    Update the current state and add dipatch and payload properties to addLocation function

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -2,14 +2,19 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) => {
   switch (action.type) {
-    default: 
-    return state;
+    case 'add_current_location':
+      return { ...state, currentLocation: action.payload };
+    default:
+      return state;
   }
 };
 
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => () => {};
+const startRecording = dispatch => () => { };
+const stopRecording = dispatch => () => { };
+const addLocation = dispatch => (location) => {
+  dispatch({ type: 'add_current_location', payload: location })
+};
+
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
