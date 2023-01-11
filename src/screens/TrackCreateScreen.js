a7f6ea221dea16555e3b0cb29749dd45efa1d843
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-10 23:39:24 -0700

    Remove unwanted imports

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,8 @@
 import '../_mockLocation';
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from 'react-native';
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy
-} from 'expo-location';
 import Map from "../components/Map";
 import { Context as LocationContext } from '../context/LocationContext';
 
