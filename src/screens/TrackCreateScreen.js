c1ec5b3757f190eeb111e1fad70482fbe5260287
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-10 23:38:11 -0700

    Move location related logic to custom hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -14,30 +14,6 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      },
-      location => {
-        addLocation(location);
-      });
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
