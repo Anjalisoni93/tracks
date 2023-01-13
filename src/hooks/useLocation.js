fdc3afd805d915a1f4cb0aa637ebbfb2c01b003d
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-12 23:37:54 -0700

    Add a new state for subscriber and set it to sub

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,19 +5,20 @@ import {
   watchPositionAsync
 } from "expo-location";
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
-      const subscriber = await watchPositionAsync({
+      const sub = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
       },
         callback
       );
-
+      setSubscriber(sub);
       const { granted } = await requestForegroundPermissionsAsync();
       if (!granted) {
         throw new Error('Location permission not granted');
@@ -28,8 +29,13 @@ export default (callback) => {
   };
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]);
 
   return [err];
 };
