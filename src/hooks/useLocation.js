4e3e8bf557c85cb2c5c36ab830d054a574c3b6e2
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-12 23:22:05 -0700

    Assign a variable named subscriber to watchPosition

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -10,7 +10,7 @@ export default (callback) => {
 
   const startWatching = async () => {
     try {
-      await watchPositionAsync({
+      const subscriber = await watchPositionAsync({
         accuracy: Accuracy.BestForNavigation,
         timeInterval: 1000,
         distanceInterval: 10
