c179a2ac42a858230ba34d5b67656d8130aa22b6
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-25 22:39:41 -0700

    Add a space around input and add a placeholder

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Input, Button } from "react-native-elements";
+import Spacer from "./Spacer";
 
 const TrackForm = () => {
-  return <>
-    <Input />
-    <Button title="Start Recording" />
-  </>
+  return (
+    <>
+      <Spacer>
+        <Input placeholder="Enter Name" />
+      </Spacer>
+      <Button title="Start Recording" />
+    </>
+  );
 };
 
 export default TrackForm;
