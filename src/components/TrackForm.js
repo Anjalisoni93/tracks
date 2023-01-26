c89071a750360b8473f5bb68719e803bd6fd64e0
Anjali Soni <anjalipatadiya@gmail.com>
2023-01-25 22:14:47 -0700

    Add a button

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Input, Button } from "react-native-elements";
 
 const TrackForm = () => {
-
+  return <>
+    <Input />
+    <Button title="Start Recording" />
+  </>
 };
 
 export default TrackForm;
