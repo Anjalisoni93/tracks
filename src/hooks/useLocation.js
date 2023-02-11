a75233220527bede43d0d662fef3e758ce65b2fb
Anjali Soni <anjalipatadiya@gmail.com>
2023-02-10 18:19:15 -0700

    Move startWatching function into useEffect and remove the useState of subscriber

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -2,38 +2,40 @@ import { useState, useEffect } from "react";
 import {
   Accuracy,
   requestForegroundPermissionsAsync,
-  watchPositionAsync
+  watchPositionAsync,
 } from "expo-location";
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const sub = await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      },
-        callback
-      );
-      setSubscriber(sub);
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+        const { granted } = await requestForegroundPermissionsAsync();
+        if (!granted) {
+          throw new Error("Location permission not granted");
+        }
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
     return () => {
