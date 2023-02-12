ae3ab41df8846d8b6235e090b9eb3a5cbe507b7a
Anjali Soni <anjalipatadiya@gmail.com>
2023-02-12 00:21:04 -0700

    Import TrackProvider

--- App.js
+++ App.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AccountScreen from './src/screens/AccountScreen';
-import SigninScreen from './src/screens/SigninScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import TrackCreateScreen from './src/screens/TrackCreateScreen';
-import TrackDetailScreen from './src/screens/TrackDetailScreen';
-import TrackListScreen from './src/screens/TrackListScreen';
-import { Provider as AuthProvider } from './src/context/AuthContext';
-import { setNavigator } from './src/navigationRef';
-import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
-import { Provider as LocationProvider } from './src/context/LocationContext';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import { Provider as AuthProvider } from "./src/context/AuthContext";
+import { setNavigator } from "./src/navigationRef";
+import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
+import { Provider as LocationProvider } from "./src/context/LocationContext";
+import { Provider as TrackProvider } from "./src/context/TrackContext";
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuth: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
-    Signin: SigninScreen
+    Signin: SigninScreen,
   }),
   mainFlow: createMaterialBottomTabNavigator({
     trackListFlow: createStackNavigator({
       TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen
+      TrackDetail: TrackDetailScreen,
     }),
     TrackCreate: TrackCreateScreen,
-    Account: AccountScreen
+    Account: AccountScreen,
   }),
 });
 
@@ -35,15 +36,17 @@ const App = createAppContainer(switchNavigator);
 export default () => {
   return (
     <SafeAreaProvider>
-      <LocationProvider>
-        <AuthProvider>
-          <App
-            ref={(navigator) => {
-              setNavigator(navigator)
-            }}
-          />
-        </AuthProvider>
-      </LocationProvider>
+      <TrackProvider>
+        <LocationProvider>
+          <AuthProvider>
+            <App
+              ref={(navigator) => {
+                setNavigator(navigator);
+              }}
+            />
+          </AuthProvider>
+        </LocationProvider>
+      </TrackProvider>
     </SafeAreaProvider>
   );
 };
